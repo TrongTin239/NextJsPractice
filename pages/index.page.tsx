7c8bb5e691a16f29dbb9e3e83f4c7d4bd73b3cb6
TrongTin <trongtin3311@gmail.com>
2023-03-01 14:48:03 +0700

    markdown

--- pages/index.page.tsx
+++ pages/index.page.tsx
@@ -1,4 +1,5 @@
 import { Box } from "@mui/material";
+import SEO from "component/common/seo";
 import FeatureWorks from "component/home/feature-works";
 import { HeroSection } from "component/home/index.page";
 import RecentPost from "component/home/recent-post.page";
@@ -12,6 +13,15 @@ export interface indexProps {
 export default function Home({ postList }: indexProps) {
   return (
     <Box>
+      <SEO
+        data={{
+          title: "NextJs Practice | Nguyễn Trọng Tín",
+          description: "Learning Nextjs  to build a website by myself",
+          thumbnailUrl:
+            "https://res.cloudinary.com/dfnh1k50l/image/upload/v1677588784/nextjs/Rectangle_30_1_wqy457.jpg",
+          url: "https://learn-nextjs-mibiz4rcc-trongtin239.vercel.app/",
+        }}
+      />
       <HeroSection />
       <RecentPost />
       <FeatureWorks />
